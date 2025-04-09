ed7d2d9d7bb26437440c20a3537c763b46c2fb4e
RaganaBloodedge <1806015345@qq.com>
2025-04-09 20:46:18 +0300

    check

--- ImmutableUnrollLinkedList_test.py
+++ ImmutableUnrollLinkedList_test.py
@@ -24,12 +24,11 @@ def test_monoid_associativity(a: list, b: list, c: list):
     assert to_list(left) == to_list(right)
 
 
-def test_IULL_api():
+def test_cons():
     empty_list = ImmutableUnrolledLinkedList[int]()
     l1 = cons(None, cons(1, empty_list))
     l2 = cons(1, cons(None, empty_list))
-
-    # cons
+    
     assert str(empty_list) == "[]"
     assert str(l1) == "[None, 1]"
     assert str(l2) == "[1, None]"
@@ -38,137 +37,150 @@ def test_IULL_api():
     assert l1 != l2
     assert l1 == cons(None, cons(1, empty_list))
 
-    # remove
 
+def test_remove():
+    empty_list = ImmutableUnrolledLinkedList[int]()
+    l1 = cons(None, cons(1, empty_list))
+    
     assert str(remove(l1, None)) == "[1]"
     assert str(remove(l1, 1)) == "[None]"
 
-    # length
+
+def test_length():
+    empty_list = ImmutableUnrolledLinkedList[int]()
+    l1 = cons(None, cons(1, empty_list))
+    
     assert length(empty_list) == 0
     assert length(l1) == 2
-    assert length(l2) == 2
 
-    # member
+
+def test_member():
+    empty_list = ImmutableUnrolledLinkedList[int]()
+    l1 = cons(None, cons(1, empty_list))
+    
     assert not member(empty_list, None)
     assert member(l1, None)
     assert member(l1, 1)
     assert not member(l1, 2)
 
-    # to_list
+
+def test_to_list():
+    empty_list = ImmutableUnrolledLinkedList[int]()
+    l1 = cons(None, cons(1, empty_list))
+    
     assert to_list(empty_list) == []
     assert to_list(l1) == [None, 1]
-    assert to_list(l2) == [1, None]
 
-    # from_list
+
+def test_from_list():
     test_data = [[], [1], [2, 3], [1, 2, 3], [None, 1]]
     for e in test_data:
         assert to_list(from_list(e)) == e
 
-    # reverse
+
+def test_reverse():
+    empty_list = ImmutableUnrolledLinkedList[int]()
+    l1 = cons(None, cons(1, empty_list))
+    l2 = cons(1, cons(None, empty_list))
+    
     assert reverse(empty_list) == empty_list
     assert reverse(l1) == l2
     assert reverse(l2) == l1
 
-    # concat and monoid_identity
+
+def test_concat():
+    empty_list = ImmutableUnrolledLinkedList[int]()
+    l1 = cons(None, cons(1, empty_list))
+    l2 = cons(1, cons(None, empty_list))
+    
     assert concat(empty_list, empty_list) == empty_list
     assert concat(l1, empty_list) == l1
     assert concat(empty_list, l1) == l1
-    print(concat(l1, l2))
-    print(from_list([None, 1, 1, None]))
     assert concat(l1, l2) == from_list([None, 1, 1, None])
 
-    # iter
+
+def test_iter():
     x = [1, 2, 3]
     lst = from_list(x, node_size=2)
-
     tmp = []
     for e in lst:
         tmp.append(e)
     assert x == tmp
     assert to_list(lst) == tmp
 
+    empty_list = ImmutableUnrolledLinkedList[int]()
     list_iterator = iter(empty_list)
     try:
-        next(list_iterator)  # get next element from empty IULL
-        assert False, "StopIteration was not raised for empty list iterator"
+        next(list_iterator)
+        assert False, "StopIteration not raised"
     except StopIteration:
         pass
 
-    # filter
+
+def test_filter():
     test_list = from_list([1, 2, 3, 4, 5])
     filtered_list = filter(test_list, lambda x: x % 2 == 0)
     assert to_list(filtered_list) == [2, 4]
 
-    empty_filtered_list = filter(empty_list, lambda x: x > 0)
-    assert to_list(empty_filtered_list) == []
+    empty_list = ImmutableUnrolledLinkedList[int]()
+    empty_filtered = filter(empty_list, lambda x: x > 0)
+    assert to_list(empty_filtered) == []
 
-    # map_list
+
+def test_map_list():
     test_list = from_list([1, 2, 3])
-    # Map function: multiply by 2
     mapped_list = map_list(test_list, lambda x: x * 2)
     assert to_list(mapped_list) == [2, 4, 6]
-    # Map function: x + 1
-    empty_mapped_list = map_list(empty_list, lambda x: x + 1)
-    assert to_list(empty_mapped_list) == []
 
-    # reduce
+    empty_list = ImmutableUnrolledLinkedList[int]()
+    empty_mapped = map_list(empty_list, lambda x: x + 1)
+    assert to_list(empty_mapped) == []
+
+
+def test_reduce():
     test_list = from_list([1, 2, 3, 4])
-    # Reduce function: sum
     sum_result = reduce(test_list, lambda acc, x: acc + x, 0)
     assert sum_result == 10
 
-    empty_reduce_result = reduce(empty_list, lambda acc, x: acc + x, 0)
-    assert empty_reduce_result == 0
+    empty_list = ImmutableUnrolledLinkedList[int]()
+    empty_reduce = reduce(empty_list, lambda acc, x: acc + x, 0)
+    assert empty_reduce == 0
 
-    # empty
-    empty_list_test = empty(node_size=3)  # Create empty list with node_size 3
-    assert empty_list_test.head_node is None
-    assert empty_list_test.node_size == 3
-    assert length(empty_list_test) == 0
-    assert to_list(empty_list_test) == []
 
-    # find
+def test_empty():
+    empty_list = empty(node_size=3)
+    assert empty_list.head_node is None
+    assert empty_list.node_size == 3
+    assert length(empty_list) == 0
+    assert to_list(empty_list) == []
+
+
+def test_find():
     test_list = from_list([1, 2, 3, 4])
-    found_element = find(test_list, lambda x: x % 2 == 0)
-    assert found_element == 2
-    # Find None in a list with None
+    assert find(test_list, lambda x: x % 2 == 0) == 2
+    
     list_with_none = from_list([1, None, 3])
-    found_none = find(list_with_none, lambda x: x is None)
-    assert found_none is None
+    assert find(list_with_none, lambda x: x is None) is None
+    
+    assert find(test_list, lambda x: x > 10) is None
+    
+    empty_list = ImmutableUnrolledLinkedList[int]()
+    assert find(empty_list, lambda x: x > 0) is None
 
-    # Element not found
-    not_found_element = find(test_list, lambda x: x > 10)
-    assert not_found_element is None
 
-    # Find in empty list
-    empty_find_result = find(empty_list, lambda x: x > 0)
-    assert empty_find_result is None
-
-    # intersection
+def test_intersection():
     list1 = from_list([1, 2, 3, 4])
     list2 = from_list([3, 4, 5, 6])
-    # Common elements [3, 4]
-    intersection_list = intersection(list1, list2)
-    assert to_list(intersection_list) == [3, 4]
-
-    # No common elements, empty list
+    assert to_list(intersection(list1, list2)) == [3, 4]
+    
     list3 = from_list([7, 8, 9])
-    no_intersection_list = intersection(list1, list3)
-    assert to_list(no_intersection_list) == []
-
-    # Intersection with self is self
-    same_list_intersection = intersection(list1, list1)
-    assert to_list(same_list_intersection) == to_list(list1)
-
-    # Intersection with empty list is empty
-    list_empty_intersection = intersection(list1, empty_list)
-    assert to_list(list_empty_intersection) == []
-
-    # Intersection of two empty lists is empty
-    empty_empty_intersection = intersection(empty_list, empty_list)
-    assert to_list(empty_empty_intersection) == []
+    assert to_list(intersection(list1, list3)) == []
+    
+    assert to_list(intersection(list1, list1)) == to_list(list1)
+    
+    empty_list = ImmutableUnrolledLinkedList[int]()
+    assert to_list(intersection(list1, empty_list)) == []
+    assert to_list(intersection(empty_list, empty_list)) == []
 
 
 if __name__ == '__main__':
-    test_IULL_api()
-    test_monoid_associativity()
